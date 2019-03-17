9becf7dc3652a9357f05f164639faa99f2a83dba
Rachel Barrett <rachel_barrett@live.co.uk>
2019-03-17 22:02:46 +0000

    Refactoring - modularising and reformatting functions

--- src/Main.elm
+++ src/Main.elm
@@ -1,59 +1,127 @@
 import Browser
 import Svg
 import Svg.Attributes exposing (cx, cy, r, fill, width, height)
-import Html
+import Html exposing (Html)
 import Html.Attributes exposing (style)
-import Svg.Events exposing (onClick, onMouseOver)
+import Svg.Events exposing (onClick, onMouseOver, onMouseOut)
 
 main = 
-  Browser.sandbox {
-    init = init, 
-    update = update, 
-    view = view
-  }
+  Browser.sandbox
+    { init = init
+    , update = update 
+    , view = view
+    }
 
-type alias Model = {
-    board: Board,
-    highlight: Maybe (Int, Int)
+-- data model --
+
+type alias Model = 
+  { board: Board
+  , highlight: Maybe (Int, Int)
   }
 type alias Board = List Int
 
-type Msg = Select (Int, Int) | Highlight (Int, Int)
+type Msg = Select (Int, Int) | Highlight (Int, Int) | UnHighlight
 
-init = {
-  board = [5,2,0,4],
-  highlight = Maybe.Nothing
+-- init --
+
+init: Model
+init = 
+  { board = [5,2,0,4]
+  , highlight = Maybe.Nothing
   }
 
-update msg model = 
+-- update --
+
+update: Msg -> Model -> Model
+update msg model =
   case msg of
     Select move -> {model | board = boardAmend move model.board}
-    Highlight highlight -> {model | highlight = Just highlight}
-
-
-view model =  circleBoard model
-
+    Highlight highlight -> {model | highlight = Maybe.Just highlight}
+    UnHighlight -> {model | highlight = Maybe.Nothing}
 
 boardAmend move board = 
-  List.indexedMap (\n -> \x -> if (n == Tuple.first move) then (Tuple.second move) else x) board
+  board
+    |> List.indexedMap (\n -> \x -> if (n == Tuple.first move) then (Tuple.second move) else x)    
 
+-- view --
 
-circleBoard model = Svg.svg [svgWidth model.board, svgHeight model.board] (List.map (circleFromCoordinate model.highlight) (coordinatesFromBoard model.board))
+view: Model -> Html Msg
+view = modelViewFromModel >> display
 
-circleFromCoordinate highlight (x, y) = Svg.circle [onClick (Select (x, y)), onMouseOver (Highlight (x, y)), cx (svgCoordinate x), cy (svgCoordinate y), r (String.fromInt(circleRadius)), fill (if (shouldHighlightCoordinate highlight (x,y)) then blueHighlight else blueColour) ] []
+  -- modelViewFromModel --
 
-coordinatesFromBoard board = List.concat (List.indexedMap (\x -> \n -> (List.map (Tuple.pair x) (List.range 0 (n-1)))) board)
+modelViewFromModel: Model -> ModelView
+modelViewFromModel model = 
+  coordinatesFromBoard model.board
+    |> withHighlights model.highlight
 
-shouldHighlightCoordinate highlight (x, y) = Maybe.withDefault False (Maybe.map (\(xh, yh) -> x == xh && y >= yh) highlight)
+coordinatesFromBoard board = 
+  board
+    |> List.indexedMap (\x -> \n -> (List.map (Tuple.pair x) (List.range 0 (n-1))))
+    |> List.concat
+
+withHighlights highlight coordinates = 
+  coordinates
+    |> List.map (\coordinate -> Tuple.pair coordinate (shouldHighlightCoordinate highlight coordinate))
+
+shouldHighlightCoordinate highlight (x, y) = 
+  case highlight of
+    Maybe.Just (xh, yh) -> x == xh && y >= yh
+    Maybe.Nothing -> False 
+      
+  -- display --
+
+display: ModelView -> Html Msg
+display modelView =
+  Svg.svg 
+    [ width (String.fromInt(boardWidth modelView)), height (String.fromInt(boardHeight modelView))]
+    (List.map circleFromCoordinateAndHighlight modelView)
+
+circleFromCoordinateAndHighlight ((x, y), highlight) = 
+  Svg.circle 
+    [ onClick (Select (x, y)) 
+    , onMouseOver (Highlight (x, y)) 
+    , onMouseOut UnHighlight
+    , cx (svgCoordinate x)
+    , cy (svgCoordinate y) 
+    , r (String.fromInt(circleRadius)) 
+    , fill (if (highlight) then blueHighlight else blueColour) 
+    ] []
+
+circleDimension = 2 * (circleRadius + circlePadding)
+boardWidth modelView = (Maybe.withDefault 0 (List.maximum (List.map (\x -> Tuple.first (Tuple.first x)) modelView)) + 1) * circleDimension
+boardHeight modelView = (Maybe.withDefault 0 (List.maximum (List.map (\x -> Tuple.second (Tuple.first x)) modelView)) + 1) * circleDimension
+svgCoordinate x = String.fromInt((circleRadius + circlePadding) * (1 + 2 * x))
 
 circleRadius = 20
 circlePadding = 5
 blueColour = "#0B79CE"
 blueHighlight = "#84c1ff"
-circleDimension = 2 * (circleRadius + circlePadding)
-boardWidth board = (List.length board) * circleDimension
-boardHeight board = (Maybe.withDefault 0 (List.maximum board)) * circleDimension
-svgWidth board = width (String.fromInt(boardWidth board))
-svgHeight board = height (String.fromInt(boardHeight board))
-svgCoordinate x = String.fromInt((circleRadius + circlePadding) * (1 + 2 * x))
+
+  -- 
+
+type alias ModelView = List ((Int, Int), Bool)
+
+-- notes --
+
+-- would be really nice to have a haskell where, that would read more naturally
+-- also annoying that you can't define types within lets
+-- need to be able to use lambda functions without brackets
+-- in haskell is it ok to use parameter names that are already bound?
+-- would be good if case classes didn't require commas
+  
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
 
